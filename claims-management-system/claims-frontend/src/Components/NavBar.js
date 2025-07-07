602c4ac2b1ce23fb4fa369ef81b52464ffb90baf
keith-oruo <keith.oruo@strathmore.edu>
2025-07-07 14:34:14 +0300

    implement logout

--- claims-management-system/claims-frontend/src/Components/NavBar.js
+++ claims-management-system/claims-frontend/src/Components/NavBar.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar({ role }) {
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+    window.location.href = '/';
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
@@ -19,6 +24,11 @@ function Navbar({ role }) {
             {role === 'SystemAdmin' && (
               <li className="nav-item"><Link className="nav-link" to="/profile">Admin Panel</Link></li>
             )}
+          </ul>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <button className="btn btn-outline-light" onClick={handleLogout}>Log Out</button>
+            </li>
           </ul>
         </div>
       </div>
